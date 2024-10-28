7afcadc9cee0095ff02d66921fe9bba55b819c6d
samj02 <sj923371@gmail.com>
2024-10-28 17:50:39 -0400

    Com

--- ray_ellipsoid_intersection.py
+++ ray_ellipsoid_intersection.py
@@ -64,6 +64,9 @@ if discr >= 0:
         d = (-b + math.sqrt(discr)) / (2 * a)
     if d > 0:
         l_d = [d * d_l_x + c_l_x, d * d_l_y + c_l_y, d * d_l_z + c_l_z]
+    else:
+        print('No Intersection ')
+        quit()
 
 print(l_d[0])  # x-component of intersection point
 print(l_d[1])  # y-component of intersection point
